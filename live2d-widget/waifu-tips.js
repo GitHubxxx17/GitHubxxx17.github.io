4578b28ca64390ef05975346c4522289327dd16e
GitHubxxx17 <s486275391@outlook.com>
2022-11-27 13:32:38 +0800

    Site updated: 2022-11-27 13:32:37

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -162,8 +162,8 @@ function loadWidget(config) {
 			modelTexturesId = localStorage.getItem("modelTexturesId");
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
-			modelId = 3; // 模型 ID1
-			modelTexturesId = 85; // 材质 ID53
+			modelId = 2; // 模型 ID1
+			modelTexturesId = 0; // 材质 ID53
 		}
 		loadModel(modelId, modelTexturesId);
 		fetch(waifuPath)
